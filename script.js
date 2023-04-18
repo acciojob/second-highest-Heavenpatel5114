83bf30d60c9099d91d4a3e7ef92b51a7aca430fd
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-04-18 09:48:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-//your JS code here. If required.
-function findSecondLargest(a, n) {
-  /*
-  First, sort the array and find the first_largest element present in the array (at the last position).
-  */
-  a.sort();
-
-  /*
-  Now for the second_largest element, we need to start from second last element as the largest element is at last.
-  */
-  let second_largest = 0;
-  /*
-  If the last and second last element are equal then check the previous one else return the second last element.
-  */
-  for (let i = n - 2; i >= 0; i--) {
-    if (a[i] != a[n - 1]) {
-      second_largest = a[i];
-      break;
+ function print2largest(arr, arr_size) {
+        let i, first, second;
+  
+        // There should be atleast two elements
+        if (arr_size < 2) {
+            document.write(" Invalid Input ");
+            return;
+        }
+  
+        // sort the array
+        arr.sort();
+  
+        // start from second last element
+        // as the largest element is at last
+        for (i = arr_size - 2; i >= 0; i--) {
+            // if the element is not
+            // equal to largest element
+            if (arr[i] != arr[arr_size - 1]) {
+                document.write("The second largest element is " + arr[i]);
+                return;
+            }
+        }
+  
+        document.write("There is no second largest element<br>");
     }
-  }
-
-  return second_largest;
-}
-
-const a = [12, 35, 1, 10, 34, 1];
-let n = a.length;
-let answer = findSecondLargest(a, n);
-console.log("The second largest element in the array is: " + answer);
+  
+    // Driver program to test above function
+ 
+    let arr= [ 12, 35, 1, 10, 34, 1 ];
+    let n = arr.length;
+    print2largest(arr, n);
